e76bf0e9baff049cdc216b6bf2b2e60da0b98830
Hugh Guiney <hugh@hughguiney.com>
2018-11-06 06:09:38 -0600

    Tweaks

--- src/script/main.js
+++ src/script/main.js
@@ -61,14 +61,21 @@ function displayTokyoTime() {
   $datetime.setAttribute( 'datetime', tokyoTime.toISOString() );
 }
 
+function playMusic() {
+  $someday.play();
+}
+
 // Init
 var $html = document.documentElement;
 $html.classList.remove( 'no-js' );
 $html.classList.add( 'js' );
 
+var $checkbox = document.getElementById( 'checkbox' );
 var $hamburger = document.getElementById( 'hamburger' );
 var $hamburgerTarget = document.getElementById( $hamburger.getAttribute( 'aria-controls' ) );
 var $trayLinks = document.getElementById( 'tray-links' );
+var $someday = document.getElementById( 'someday' );
+var $volume = document.getElementById( 'volume' );
 
 var KEY_ENTER = 13;
 var KEY_SPACE = 32;
@@ -94,6 +101,17 @@ function setExpandedMenuState( $button, $toggleTarget ) {
   $button.setAttribute( 'aria-expanded', 'true' );
   $toggleTarget.setAttribute( 'aria-hidden', 'false' );
   putTrayLinksIntoTabOrder();
+
+  setTimeout( function () {
+    document.body.addEventListener( 'click', collapseHamburgerMenu );
+  }, 0 );
+}
+
+function expandHamburgerMenu() {
+  if ( $hamburger.getAttribute( 'aria-expanded' ) === 'false' ) {
+    $checkbox.checked = true;
+    setExpandedMenuState( $hamburger, $hamburgerTarget );
+  }
 }
 
 function setCollapsedMenuState( $button, $toggleTarget ) {
@@ -102,13 +120,24 @@ function setCollapsedMenuState( $button, $toggleTarget ) {
   $button.setAttribute( 'aria-expanded', 'false' );
   $toggleTarget.setAttribute( 'aria-hidden', 'true' );
   takeTrayLinksOutOfTabOrder();
+
+  setTimeout( function () {
+    document.body.removeEventListener( 'click', collapseHamburgerMenu );
+  }, 0 );
+}
+
+function collapseHamburgerMenu() {
+  if ( $hamburger.getAttribute( 'aria-expanded' ) === 'true' ) {
+    $checkbox.checked = false;
+    setCollapsedMenuState( $hamburger, $hamburgerTarget );
+  }
 }
 
 function hamburgerKeyHandler( event ) {
   switch ( event.which ) {
     case KEY_ENTER:
     case KEY_SPACE: {
-      event.stopPropagation;
+      event.stopPropagation();
 
       if ( $hamburger.getAttribute( 'aria-expanded' ) === "false" ) {
         $hamburger.click();
@@ -134,10 +163,28 @@ function hamburgerClickHandler( event ) {
   }
 }
 
+function isPlaying( media ) {
+  return !!( media.currentTime > 0 && !media.paused && !media.ended && media.readyState > 2 );
+}
+
 takeTrayLinksOutOfTabOrder();
 
 $hamburger.addEventListener( 'keypress', hamburgerKeyHandler );
 $hamburger.addEventListener( 'click', hamburgerClickHandler );
+
+$volume.addEventListener( 'click', function ( event ) {
+  switch ( $volume.textContent ) {
+    case '🔊':
+      $volume.textContent = $volume.getAttribute( 'data-muted-icon' );
+      $someday.muted = true;
+    break;
+
+    case '🔇':
+      $volume.textContent = $volume.getAttribute( 'data-playing-icon' );
+      $someday.muted = false;
+    break;
+  }
+} );
 
 // Date & time
 var $currentTime = document.getElementById( 'current-time' );
@@ -158,3 +205,15 @@ setTimeout( function () {
 }, 1000 );
 
 setInterval( displayTokyoTime, 1000 );
+
+function startMusic() {
+  if ( !isPlaying( $someday ) ) {
+    playMusic();
+  }
+
+  window.removeEventListener( 'scroll', startMusic );
+  document.removeEventListener( 'click', startMusic );
+}
+
+window.addEventListener( 'scroll', startMusic );
+document.addEventListener( 'click', startMusic );
